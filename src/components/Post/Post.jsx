4a2c1b1b918fd08c3223a5dda9f8b2390ff89441
Mariam.ajattar <mariam.ajattar@student.vinci.be>
2022-11-24 20:12:18 +0100

    Post.jsx

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,4 +1,5 @@
 import {Image, Linking, ScrollView, Text} from "react-native";
+import {Icon, Button, ListItem, Card} from '@rneui/themed';
 
 
 const Post = ({post}) => {
@@ -6,33 +7,36 @@ const Post = ({post}) => {
 
     return (
         post.isVideo ?
-            <>
-                <Text>
-                    Subreddit : {post.subreddit_name_prefixed}
-                </Text>
-                <Text>Title : {post.title}</Text>
-                <Text>Author : {post.author}</Text>
-                <Text
-                    style={{color: 'blue'}}
-                    onPress={() => Linking.openURL(post.url)}>URL</Text>
-                <Text>+ {post.pwls} - {post.downs}</Text>
-                <Text>url_overridden_by_dest : {post.url_overridden_by_dest}</Text>
-            </>
-            :
             <ScrollView>
                 <Text>
-                    Subreddit : {post.subreddit_name_prefixed}
+                    Subreddit isVideo: {post.subreddit_name_prefixed}
                 </Text>
                 <Text>Title : {post.title}</Text>
                 <Text>Author : {post.author}</Text>
                 <Text
                     style={{color: 'blue'}}
                     onPress={() => Linking.openURL(post.url)}>URL</Text>
-                <Text>+ {post.pwls} - {post.downs}</Text>
-                <Text>Text : {post.selftext}</Text>
-                <Text>thumbnail : </Text><Image source={post.thumbnail} />
+                <Text>url_overridden_by_dest : {post.url_overridden_by_dest}</Text>
+            </ScrollView>
+            :
+            <ScrollView>
+                    <Text>
+                        Author : {post.author}
+                    </Text>
+                    <Text>
+                        Subreddit : {post.subreddit_name_prefixed}
+                    </Text>
+                    <Image style={{
+                        width: post.thumbnail * 2, 
+                        height : post.thumbnail *2, 
+                        marginVertical: 10,
+                    }}
+                    resizeMode="cover"
+                    source={{uri:post.thumbnail}}/>
+                    <Text>{post.title}</Text>
             </ScrollView>
     );
 };
 
-export {Post};
+
+export default Post
